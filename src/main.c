b9b0f79b097960252f372bf2761ab0f3ec59f90b
PWUGA <tihongorbach@gmail.com>
2018-09-15 17:43:20 +0700

    hotfix: add return for main

--- src/main.c
+++ src/main.c
@@ -7,4 +7,5 @@ int main() {
     printf ("Bulls and cows\n");
     printf ("Game rules.\n\nYour task is to guess the number that made the computer\n");
     printf ("-->Guessed the number in the right place: 1 bull. \n-->Guessed the number in the wrong place: 1 cow. \n\nOnce you have guessed the number, the game ends.\n");
+    return 0;
 }
