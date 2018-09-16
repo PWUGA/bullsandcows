05b7eceaefb6e10897340f218daf2967fc1d565f
3-14vko <sparshukov@yandex.ru>
2018-09-16 17:10:32 +0700

    Add tests for input checker function

--- src/main_test.c
+++ src/main_test.c
@@ -107,6 +107,60 @@ CTEST (Counter, CowsCorrect3)
 	ASSERT_EQUAL (exp, real);
 }
 
+CTEST (InputChecker, Correct)
+{
+	int exp;
+	int real;
+	char *input = malloc (sizeof (char));
+	exp = 1234;
+	strcpy (input, "1234");
+	real = checkInput (input);
+	ASSERT_EQUAL (exp, real);
+}
+
+CTEST (InputChecker, Incorrect)
+{
+	int exp;
+	int real;
+	char *input = malloc (sizeof (char));
+	exp = 1234;
+	strcpy (input, "s1234");
+	real = checkInput (input);
+	ASSERT_NOT_EQUAL (exp, real);
+}
+
+CTEST (InputChecker, Incorrect2)
+{
+	int exp;
+	int real;
+	char *input = malloc (sizeof (char));
+	exp = 300;
+	strcpy (input, "300");
+	real = checkInput (input);
+	ASSERT_NOT_EQUAL (exp, real);
+}
+
+CTEST (InputChecker, Incorrect3)
+{
+	int exp;
+	int real;
+	char *input = malloc (sizeof (char));
+	exp = 300;
+	strcpy (input, "10000");
+	real = checkInput (input);
+	ASSERT_NOT_EQUAL (exp, real);
+}
+
+CTEST (InputChecker, Incorrect4)
+{
+	int exp;
+	int real;
+	char *input = malloc (sizeof (char));
+	exp = 0;
+	strcpy (input, "/qw&123$$$^%@356gfh&");
+	real = checkInput (input);
+	ASSERT_EQUAL (exp, real);
+}
 int main (int argc, const char** argv)
 {
 	return ctest_main (argc, argv);
